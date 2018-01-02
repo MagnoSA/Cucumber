d830580f00086e66b33eda8ad0bcf279c8eb5942
MagnoSA <rodrigomsazevedo@gmail.com>
2018-01-02 13:44:23 -0200

    Teste

--- forca/src/teste.rb
+++
@@ -1,8 +0,0 @@
-
-nome = 'Rodrigo'
-idade = 28
-bonito = true
-
-puts nome.class
-puts idade.class
-puts bonito.class
--- soma/tests/features/step_definitions/soma_steps.rb
+++
@@ -1,9 +0,0 @@
-#encoding: utf-8
-
-Quando(/^faço a soma de (\d+) \+ (\d+)$/) do |v1, v2|
-  @soma = v1.to_i + v2.to_i
-end
-
-Entao(/^o resultado é (\d+)$/) do |result|
-  expect(@soma).to eql result.to_i
-end
